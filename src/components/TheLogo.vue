1c74dd5eaded2bae6eaaac183c70062b760ebe1c
Daniel Roe <daniel@roe.dev>
2022-08-30 23:19:45 +0100

    fix: use current logo

--- src/components/TheLogo.vue
+++ src/components/TheLogo.vue
@@ -1,12 +1,5 @@
 <template>
   <span class="flex flex-row items-end">
-    <img height="24" width="36" src="~/assets/emblem.svg" alt="" />
-    <img
-      class="ml-2"
-      width="58"
-      height="20"
-      src="~/assets/logomark.svg"
-      alt="Nuxt"
-    />
+    <img width="130" height="39" src="/nuxt.svg" alt="Nuxt" />
   </span>
 </template>
