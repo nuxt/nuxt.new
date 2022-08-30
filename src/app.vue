bfb190fe6c9f9e289d6b83f03b45863f35f14e6f
Daniel Roe <daniel@roe.dev>
2022-08-31 00:33:04 +0100

    fix: remove themes for now

--- src/app.vue
+++ src/app.vue
@@ -25,7 +25,7 @@ useHead({
   <Html class="bg-black text-white" lang="en" />
   <div class="relative max-w-5xl w-full mx-auto px-6">
     <Spotlight class="z-[1]" />
-    <div class="z-[2] relative">
+    <div class="z-[2] relative flex flex-col min-h-screen">
       <TheHeader class="xl:mt-4" />
       <h1 class="font-bold text-center text-3xl mt-12 md:text-left md:text-5xl">
         Start a Nuxt project
@@ -41,11 +41,12 @@ useHead({
         Explore our different starters with different presets.
       </p>
       <TheStarterGrid class="mt-4 md:mt-8" />
-      <h2 class="font-bold text-2xl md:text-3xl mt-20 md:mt-24">Themes</h2>
+      <!-- <h2 class="font-bold text-2xl md:text-3xl mt-20 md:mt-24">Themes</h2>
       <p class="font-thin max-w-[50ch]">
         Start with our content-driven open-source themes.
       </p>
-      <TheThemeGrid class="mt-4 md:mt-8" />
+      <TheThemeGrid class="mt-4 md:mt-8" /> -->
+      <span class="flex-grow" />
       <TheHeader class="mt-12" />
     </div>
   </div>
