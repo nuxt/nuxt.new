36a4592e6b57dc657ab26c3d8578a895b4efb179
Daniel Roe <daniel@roe.dev>
2022-08-31 10:07:48 +0100

    feat: use `@kevinmarrec/nuxt-pwa` to handle meta/icons (#5)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,9 +20,42 @@ export default defineNuxtConfig({
     '@nuxtjs/critters',
     '@nuxt/image-edge',
     'vue-plausible',
+    '@kevinmarrec/nuxt-pwa',
     '~/modules/purge-comments',
     '~/modules/zero-js',
   ],
+  pwa: {
+    meta: {
+      name: 'Start a Nuxt project',
+      description: 'The best way to get started with a new Nuxt project.',
+      theme_color: '#000',
+      ogHost: 'https://nuxt.new',
+      ogImage: {
+        path: '/social-preview.png',
+        width: 1280,
+        height: 640
+      },
+    },
+    manifest: {
+      name: 'Start a Nuxt project',
+      background_color: '#000',
+      short_name: 'nuxt.new',
+      theme_color: '#000',
+      description: 'The best way to get started with a new Nuxt project.',
+    },
+    workbox: false,
+  },
+  app: {
+    head: {
+      link: ['Light', 'Regular', 'Bold'].map(weight => ({
+        rel: 'preload',
+        as: 'font',
+        type: 'font/woff2',
+        crossorigin: '',
+        href: `/fonts/RoobertPRO-${weight}.woff2`,
+      })),
+    }
+  },
   plausible: {
     domain: 'nuxt.new'
   },
--- src/app.vue
+++ src/app.vue
@@ -1,28 +1,5 @@
-<script setup>
-useHead({
-  title: 'Start a new Nuxt project',
-  link: ['Light', 'Regular', 'Bold'].map(weight => ({
-    rel: 'preload',
-    as: 'font',
-    type: 'font/woff2',
-    crossorigin: '',
-    href: `/fonts/RoobertPRO-${weight}.woff2`,
-  })),
-  meta: [
-    {
-      name: 'description',
-      content: 'The best way to get started with a new Nuxt project.',
-    },
-    {
-      name: 'og:image',
-      content: '/social-preview.png',
-    },
-  ],
-})
-</script>
-
 <template>
-  <Html class="bg-black text-white" lang="en" />
+  <Html class="bg-black text-white" />
   <div class="relative max-w-5xl w-full mx-auto px-6">
     <Spotlight class="z-[1]" />
     <div class="z-[2] relative flex flex-col min-h-screen">
@@ -33,8 +10,8 @@ useHead({
       <p
         class="font-thin mt-6 text-center mx-auto md:mx-0 md:mt-8 md:text-left max-w-[30ch] md:max-w-[50ch]"
       >
-        Open a Nuxt starter on CodeSandbox or StackBlitz to get up and
-        running in a few seconds.
+        Open a Nuxt starter on CodeSandbox or StackBlitz to get up and running
+        in a few seconds.
       </p>
       <h2 class="font-bold text-2xl md:text-3xl mt-20 md:mt-24">Starters</h2>
       <p class="font-thin max-w-[50ch]">
