02dc1e262ca404a0b0b2e498edd65d585cf8345d
Daniel Roe <daniel@roe.dev>
2022-08-31 09:23:26 +0100

    feat: use `@kevinmarrec/nuxt-pwa` to handle meta/icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,9 +20,35 @@ export default defineNuxtConfig({
     '@nuxtjs/critters',
     '@nuxt/image-edge',
     'vue-plausible',
+    '@kevinmarrec/nuxt-pwa',
     '~/modules/purge-comments',
     '~/modules/zero-js',
   ],
+  pwa: {
+    meta: {
+      name: 'Start a new Nuxt project',
+      description: 'The best way to get started with a new Nuxt project.',
+      ogHost: 'https://nuxt.new',
+      ogImage: {
+        path: '/social-preview.png',
+        width: 1280,
+        height: 640
+      }
+    },
+    manifest: { enabled: false },
+    workbox: { enabled: false }
+  },
+  app: {
+    head: {
+      link: ['Light', 'Regular', 'Bold'].map(weight => ({
+        rel: 'preload',
+        as: 'font',
+        type: 'font/woff2',
+        crossorigin: '',
+        href: `/fonts/RoobertPRO-${weight}.woff2`,
+      })),
+    }
+  },
   plausible: {
     domain: 'nuxt.new'
   },
