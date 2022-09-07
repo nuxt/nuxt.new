efdbff0903c98aaa428b1e76a8977b3e7d4fb81a
Daniel Roe <daniel@roe.dev>
2022-09-07 15:18:22 +0100

    fix: remove console log

--- src/components/RepoButtons.vue
+++ src/components/RepoButtons.vue
@@ -51,7 +51,7 @@ const command = `npx nuxi init ${template}app`
     </a>
     <button
       class="flex gap-2 items-center px-3 py-2 rounded border-[1px] border-white border-opacity-[0.1] hover:border-opacity-[0.7] hover:bg-opacity-[0.2] transition-all col-span-2 opacity-75"
-      :onclick="`navigator.clipboard.writeText('${command}').then(r => console.log('done'))`"
+      :onclick="`navigator.clipboard.writeText('${command}')`"
     >
       <span class="sr-only">Click to copy</span>
       <code class="flex items-center gap-2">
