0a215a1712bbfec5f56601460d733fefe0b8607f
Daniel Roe <daniel@roe.dev>
2022-09-07 17:05:18 +0100

    perf: adopt sprite map to reduce resource downloading (#13)

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,25 +1,16 @@
 <template>
   <header class="py-4 flex items-center w-full">
     <a target="_blank" href="https://nuxtjs.org">
-      <TheLogo />
+      <span class="sr-only">Nuxt</span>
+      <svg width="130" height="39" alt=""><use xlink:href="#nuxt" /></svg>
     </a>
     <a target="_blank" href="https://twitter.com/nuxt_js" class="ml-auto p-2">
-      <img
-        src="/logos/twitter.svg"
-        class="w-5 h-5"
-        height="20"
-        width="20"
-        alt="Twitter"
-      />
+      <span class="sr-only">Nuxt Twitter account</span>
+      <svg class="w-5 h-5" alt=""><use xlink:href="#twitter" /></svg>
     </a>
     <a target="_blank" href="https://github.com/nuxt" class="p-2">
-      <img
-        src="/logos/github.svg"
-        class="w-5 h-5"
-        height="20"
-        width="20"
-        alt="GitHub"
-      />
+      <span class="sr-only">Nuxt GitHub organization</span>
+      <svg class="w-5 h-5" alt=""><use xlink:href="#github" /></svg>
     </a>
   </header>
 </template>
--- src/components/TheLogo.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <span class="flex flex-row items-end">
-    <img width="130" height="39" src="/nuxt.svg" alt="Nuxt" />
-  </span>
-</template>
