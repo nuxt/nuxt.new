fc770b624edc16be6cb1a48363d55d7d77772bc5
Daniel Roe <daniel@roe.dev>
2022-09-08 12:21:23 +0100

    feat: add github link to repo card (#12)

--- src/components/TheStarterGrid.vue
+++ src/components/TheStarterGrid.vue
@@ -9,7 +9,24 @@ const { data: items } = await useFetch('/data/starters.json')
       class="rounded-lg border-gray-800 border-[1px] px-4 py-4 bg-gradient-to-r from-[rgba(24,24,27,0.65)] backdrop-blur-sm to-transparent"
     >
       <img :src="item.image" class="h-8 w-8" alt="" />
-      <div class="font-bold mt-4">{{ item.name }}</div>
+      <div class="font-bold mt-4 flex flex-row gap-2 items-center">
+        <span>
+          {{ item.name }}
+        </span>
+        <a
+          class="opacity-50 hover:opacity-100 transition-opacity"
+          :href="`https://github.com/${item.repo}/tree/${item.branch}${
+            item.dir ? `/${item.dir}` : ''
+          }`"
+        >
+          <svg
+            class="w-4 h-4"
+            :alt="`GitHub repository for ${item.name} starter`"
+          >
+            <use xlink:href="#github" />
+          </svg>
+        </a>
+      </div>
       <p class="font-thin">{{ item.description }}</p>
       <RepoButtons :repo="item.repo" :branch="item.branch" :dir="item.dir" />
     </article>
