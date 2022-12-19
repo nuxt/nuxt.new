248bf757ce92ebb9867076799406ef0f95e74e0a
Daniel Roe <daniel@roe.dev>
2022-12-19 11:33:44 +0000

    fix: don't use deprecated syntax

--- src/server/middleware/theme.get.ts
+++ src/server/middleware/theme.get.ts
@@ -2,7 +2,7 @@ export default defineLazyEventHandler(async () => {
   const themes = new Set(['docus', 'alpine', /* 'elements' */])
 
   return defineEventHandler((event) => {
-    const slug = event.req.url?.slice(1)
+    const slug = event.node.req.url?.slice(1)
     if (slug && themes.has(slug)) {
       return sendRedirect(event, `https://${slug}.nuxt.dev/?studio`, 302)
     }
