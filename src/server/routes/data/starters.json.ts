f16b2bfc566e8ef04cfa37341994592b206d99c9
Daniel Roe <daniel@roe.dev>
2022-12-20 12:57:47 +0000

    feat: link to docs for each starter (#24)

--- src/server/routes/data/starters.json.ts
+++ src/server/routes/data/starters.json.ts
@@ -5,7 +5,8 @@ export default defineEventHandler((): Starter[] => [
     "description": "Minimal starter with a single app.vue.",
     "image": "data:image/svg+xml,%3Csvg width='124' height='124' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M55.75 27.155c-3.222-5.54-11.278-5.54-14.5 0L6.134 87.535C2.912 93.075 6.94 100 13.384 100h27.413c-2.753-2.407-3.773-6.57-1.69-10.142L65.704 44.27 55.75 27.155Z' fill='%2380EEC0'/%3E%3Cpath d='M78 40.4c2.667-4.533 9.333-4.533 12 0L119.06 89.8c2.667 4.533-.666 10.199-5.999 10.199H54.938c-5.333 0-8.666-5.666-6-10.199L78 40.4Z' fill='%2300DC82'/%3E%3C/svg%3E",
     "repo": "nuxt/starter",
-    "branch": "v3"
+    "branch": "v3",
+    "docs": "https://nuxt.com/docs/getting-started/installation"
   },
   {
     "name": "Content",
@@ -13,7 +14,8 @@ export default defineEventHandler((): Starter[] => [
     "description": "Starter for a content-driven website.",
     "image": "data:image/svg+xml,%3Csvg width='34' height='34' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 14h10a1.667 1.667 0 0 0 1.667-1.667v-10A1.667 1.667 0 0 0 22 .667H12a1.667 1.667 0 0 0-1.667 1.666v10A1.667 1.667 0 0 0 12 14Zm1.667-10h6.666v6.667h-6.666V4ZM32 10.667h-3.333a1.667 1.667 0 0 0 0 3.333H32a1.667 1.667 0 1 0 0-3.333Zm-3.333-3.334H32A1.667 1.667 0 1 0 32 4h-3.333a1.667 1.667 0 0 0 0 3.333ZM2 7.333h3.333a1.667 1.667 0 1 0 0-3.333H2a1.667 1.667 0 1 0 0 3.333ZM2 14h3.333a1.667 1.667 0 1 0 0-3.333H2A1.667 1.667 0 0 0 2 14Zm30 3.333H2a1.667 1.667 0 1 0 0 3.334h30a1.667 1.667 0 1 0 0-3.334ZM18.667 24H2a1.667 1.667 0 1 0 0 3.333h16.667a1.667 1.667 0 0 0 0-3.333Z' fill='%2300BD6F'/%3E%3C/svg%3E",
     "repo": "nuxt/starter",
-    "branch": "doc-driven"
+    "branch": "doc-driven",
+    "docs": "https://content.nuxtjs.org/guide/writing/document-driven"
   },
   {
     "name": "Module",
@@ -21,7 +23,8 @@ export default defineEventHandler((): Starter[] => [
     "description": "Starter to create your first Nuxt module.",
     "image": "data:image/svg+xml,%3Csvg width='34' height='34' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M29.117 9.283V9.15l-.1-.25a1.186 1.186 0 0 0-.117-.15 1.571 1.571 0 0 0-.15-.2l-.15-.117-.267-.133-12.5-7.717a1.667 1.667 0 0 0-1.766 0L1.667 8.3l-.15.133-.15.117c-.056.063-.106.13-.15.2a1.183 1.183 0 0 0-.117.15l-.1.25v.133c-.016.144-.016.29 0 .434v14.566a1.667 1.667 0 0 0 .783 1.417l12.5 7.717a.784.784 0 0 0 .25.1h.134c.282.09.584.09.866 0h.134a.784.784 0 0 0 .25-.1L28.333 25.7a1.667 1.667 0 0 0 .784-1.417V9.717c.016-.144.016-.29 0-.434ZM13.333 29.017 4.167 23.35V12.717l9.166 5.65v10.65ZM15 15.483 5.667 9.717 15 3.967l9.333 5.75L15 15.483Zm10.833 7.867-9.166 5.667v-10.65l9.166-5.65V23.35Z' fill='%2300BD6F'/%3E%3C/svg%3E",
     "repo": "nuxt/starter",
-    "branch": "module"
+    "branch": "module",
+    "docs": "https://nuxt.com/docs/guide/going-further/modules"
   },
   {
     "name": "Layer",
@@ -29,6 +32,7 @@ export default defineEventHandler((): Starter[] => [
     "description": "Starter to create your first Nuxt layer.",
     "image": "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' aria-hidden='true' role='img' width='1em' height='1em' viewBox='0 0 24 24' data-v-047565da=''%3E%3Cpath fill='%2300BD6F' d='m2.5 8.86l9 5.2a1 1 0 0 0 1 0l9-5.2A1 1 0 0 0 22 8a1 1 0 0 0-.5-.87l-9-5.19a1 1 0 0 0-1 0l-9 5.19A1 1 0 0 0 2 8a1 1 0 0 0 .5.86ZM12 4l7 4l-7 4l-7-4Zm8.5 7.17L12 16l-8.5-4.87a1 1 0 0 0-1.37.37a1 1 0 0 0 .37 1.36l9 5.2a1 1 0 0 0 1 0l9-5.2a1 1 0 0 0 .37-1.36a1 1 0 0 0-1.37-.37Zm0 4L12 20l-8.5-4.87a1 1 0 0 0-1.37.37a1 1 0 0 0 .37 1.36l9 5.2a1 1 0 0 0 1 0l9-5.2a1 1 0 0 0 .37-1.36a1 1 0 0 0-1.37-.37Z'%3E%3C/path%3E%3C/svg%3E",
     "repo": "nuxt/starter",
-    "branch": "layer"
+    "branch": "layer",
+    "docs": "https://nuxt.com/docs/guide/going-further/layers"
   }
 ])
