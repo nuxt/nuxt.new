86aafbb1dceeb01d00fee18c14f89edfe3b7d266
Florent Delerue <florentdelerue@hotmail.com>
2023-07-19 15:05:35 +0200

    refactor(hero): update spacing

--- components/Hero.vue
+++ components/Hero.vue
@@ -8,7 +8,7 @@ defineProps({
 </script>
 
 <template>
-  <div class="grid py-[124px] items-center justify-center grid-cols-1 md:grid-cols-7 gap-16 w-full">
+  <div class="grid py-[60px] sm:py-[124px] items-center justify-center grid-cols-1 md:grid-cols-7 gap-16 w-full">
     <div class="flex flex-col gap-y-4 md:col-span-4">
       <h1 class="text-4xl md:text-5xl lg:text-6xl font-bold text-white lg:leading-[64px]">
         <span class="text-green-400">Kickstart your project</span><br />
