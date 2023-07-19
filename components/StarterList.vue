6035dff57e3158e719b29e4dbf914a5dcd5dac90
Florent Delerue <florentdelerue@hotmail.com>
2023-07-19 15:06:01 +0200

    refactor(list): update spacing

--- components/StarterList.vue
+++ components/StarterList.vue
@@ -3,7 +3,7 @@ const { data: starters } = await useFetch<Array<Starter>>('/data/starters.json')
 </script>
 
 <template>
-  <ul class="grid gap-8 grid-cols-1 md:grid-cols-2">
+  <ul class="grid gap-8 grid-cols-1 md:grid-cols-2 py-[60px]">
     <StarterCard v-for="starter in starters" :key="starter.name" :starter="starter" />
   </ul>
 </template>
--- components/ThemeList.vue
+++ components/ThemeList.vue
@@ -3,7 +3,7 @@ const { data: themes } = await useFetch<Array<Theme>>('/data/themes.json')
 </script>
 
 <template>
-  <ul class="grid gap-8 grid-cols-1 md:grid-cols-2">
+  <ul class="grid gap-8 grid-cols-1 md:grid-cols-2 py-[60px]">
     <ThemeCard v-for="theme in themes" :key="theme.name" :theme="theme" />
   </ul>
 </template>
