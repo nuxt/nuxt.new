c0e543dfc8b01edb449ea900e0d99eadc6acc62d
Florent Delerue <florentdelerue@hotmail.com>
2023-07-21 14:54:48 +0200

    test lighthouse perf

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,8 +18,8 @@ export default defineNuxtConfig({
   ],
   image: {
     screens: {
-      cover: 685,
-      cover2x: 1370
+      sm: 640,
+      lg: 1024
     }
   },
   fontMetrics: {
@@ -37,5 +37,5 @@ export default defineNuxtConfig({
   },
   colorMode: {
     preference: 'dark',
-  }
+  },
 })
