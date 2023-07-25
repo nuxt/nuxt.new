a1786f95eba6e40004a9e5b396b8268f881a4b5d
Daniel Roe <daniel@roe.dev>
2023-07-25 14:12:28 +0100

    fix: add `og:title`s

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 useSeoMeta({
   title: 'Start a new Nuxt project',
+  ogTitle: 'Start a new Nuxt project',
   description: 'Open a Nuxt starter on CodeSandbox, StackBlitz or locally to get up and running in a few seconds.',
   ogSiteName: 'Nuxt.new',
   ogImage: 'https://nuxt.new/social-card.png',
--- pages/themes.vue
+++ pages/themes.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 useSeoMeta({
   title: 'Start with a Nuxt theme',
+  ogTitle: 'Start with a Nuxt theme',
   description: 'Open a Nuxt theme on CodeSandbox, StackBlitz or locally to get up and running in a few seconds.',
   ogSiteName: 'Nuxt.new',
   ogImage: 'https://www.nuxt.new/social-card-themes.png',
