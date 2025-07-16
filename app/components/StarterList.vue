18052d17a9e4a5ac48355a1138e832b1f5e4e56c
OrbisK <robin.kehl@singular-it.de>
2025-07-16 15:50:29 +0200

    feat: add deprecated starters to maintain backwards compatibility

--- app/components/StarterList.vue
+++ app/components/StarterList.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 const { data: starters } = await useFetch<Array<Starter>>('/data/starters.json')
+
+const visibleStarters = computed(() => {
+  return starters.value.filter(starter => !starter.deprecated)
+})
 </script>
 
 <template>
   <ul class="grid gap-8 grid-cols-1 md:grid-cols-2 py-[60px]">
     <StarterCard
-      v-for="starter in starters"
+      v-for="starter in visibleStarters"
       :key="starter.name"
       :starter="starter"
     />
