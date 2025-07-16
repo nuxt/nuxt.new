c57020d9b5cc6f05c1ff6e95d62890c7233532db
Daniel Roe <daniel@roe.dev>
2025-07-16 16:38:52 +0200

    chore: use transform

--- app/components/StarterList.vue
+++ app/components/StarterList.vue
@@ -1,15 +1,13 @@
 <script setup lang="ts">
-const { data: starters } = await useFetch<Array<Starter>>('/data/starters.json')
-
-const visibleStarters = computed(() => {
-  return starters.value?.filter(starter => !starter.deprecated) || []
+const { data: starters } = await useFetch<Array<Starter>>('/data/starters.json', {
+  transform: starters => starters.filter(starter => !starter.deprecated),
 })
 </script>
 
 <template>
   <ul class="grid gap-8 grid-cols-1 md:grid-cols-2 py-[60px]">
     <StarterCard
-      v-for="starter in visibleStarters"
+      v-for="starter in starters"
       :key="starter.name"
       :starter="starter"
     />
