1e4023d5887a9c847f3c3b76649498485c2fe86f
Maxime Pauvert <maximepvrt@gmail.com>
2026-02-08 18:26:00 +0100

    fix: use template list form nuxt-starter

--- shared/types/index.ts
+++ shared/types/index.ts
@@ -1,12 +1,13 @@
 export interface Starter {
   name: string
   slug: string
+  default?: boolean
   description: string
   image?: string
-  repo: string
-  branch: string
-  dir?: string
-  demo?: string
-  docs?: string
+  tar: string
+  repo: string,
+  branch: string,
+  defaultDir?: string
+  url?: string
   deprecated?: boolean
 }
